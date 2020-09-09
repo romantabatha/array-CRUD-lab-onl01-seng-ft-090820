87e0ac131b21c5ed26a03deffc656cdde3bedaa9
Tabatha Roman <romantabatha17@gmail.com>
2020-09-09 21:39:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,6 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  canadian_artist = ["The Weeknd","Drake","Justin Bieber","Celine Dion"]
   canadian_artist << "Avril Lavigne"
 end
 
