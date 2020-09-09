f2b868a408d0d19c4604d4f775c5275fef2f5649
Tabatha Roman <romantabatha17@gmail.com>
2020-09-09 21:45:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  canadian_artist = ["wow","The Weeknd","Drake","Justin Bieber","arrays!"]
+  canadian_artist.shift
 end
 
 def retrieve_element_from_index(array, index_number)
