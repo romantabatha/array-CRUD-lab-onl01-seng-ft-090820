f382f82b318a90eb7b6bb65ed69a8a57ce255ffb
Tabatha Roman <romantabatha17@gmail.com>
2020-09-09 21:41:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  canadian_artist = ["The Weeknd","Drake","Justin Bieber","Celine Dion"]
+  canadian_artist.push("wow")
 end
 
 def remove_element_from_end_of_array(array)
